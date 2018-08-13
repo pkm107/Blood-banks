28320464c7936da4f05f34b493d6bf2d60040466
pkm107 <31405097+pkm107@users.noreply.github.com>
2018-08-13 15:58:31 +0530

    Check

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 
-app.use(express.static('/app/dist'));
+app.use(express.static('/app/dist/MyProj'));
 
 // Start the app by listening on the default
 // Heroku port
